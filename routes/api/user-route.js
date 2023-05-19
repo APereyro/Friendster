659e320773799207afa93f1d76549723b3c5ac3f
Alan Pereyro <alanpereyro0@gmail.com>
2023-05-19 10:03:10 -0400

    added a create,post,findById for user routes

---
+++ routes/api/user-route.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../../models/User");
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const db = require("./config/connection");
-const routes = require("./routes");
+const userRoutes = require("./routes/api/user-route")
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// mongoose.connect('mongodb://localhost:27017/friendster', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+app.use('/api/users', userRoutes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
